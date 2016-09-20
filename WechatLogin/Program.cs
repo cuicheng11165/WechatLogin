d4461df27601c24092839ebac5c42e4affdbc878
Cheng Cui <cuicheng11165@gmail.com>
2016-09-20 22:23:15 +0800

    add

---
+++ WechatLogin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace WechatLogin
+{
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+
+            var login = new WechatCredential();
+            login.Login();
+
+
+          
+
+
+        }
+    }
+}
